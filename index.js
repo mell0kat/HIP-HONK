482a2589d4fa70111a2a7b37f402ef6634e8ebca
Katherine Mello <katherine.mello8@gmail.com>
2018-02-05 21:24:35 -0500

    Each demo resets

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const getAndPlayAudio = (rapper, instant = false, end=false) => {
 		rapperNoise.pause()
 		return
 	}
-	setTimeout(() => rapperNoise.play(), (instant ? 0 : 1750))
 
-	document.getElementById('car1').className = 'car_go'
+	const car1 = document.getElementById('car1')
+	const car2 = document.getElementById('car2')
+	setTimeout(() => rapperNoise.play(), (instant ? 0 : 1500))
+	if (!instant && !end) {
+		setTimeout(() => {
+			console.log('resetting')
+			car1.classList.remove('car_go')
+			car2.classList.remove('car_2_go')
+		}, (instant ? 0 : 4000))
+	}
+	car1.className = 'car_go'
 
-	document.getElementById('car2').className = 'car_2_go'
+	car2.className = 'car_2_go'
 
 }
 
